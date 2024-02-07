025e3cd7245d74d0274c9fc85a73aba7eebdf487
Josh Edwards <joshredwards@users.noreply.github.com>
2024-02-07 14:17:44 +1100

    [patch] adds jest with refactored test

--- src/components/tool/components/show-example/utils/example-to-string/index.ts
+++ src/components/tool/components/show-example/utils/example-to-string/index.ts
@@ -1,24 +1,41 @@
-import { isArray, isString, map } from "lodash/fp";
+import { isArray, isString, map } from 'lodash/fp';
 
-const exampleToString = (blocks = []) => {
+type Asset = {
+  _ref: string;
+  _type: string;
+};
+
+type Block = {
+  current?: string;
+  _type: string;
+  asset?: Asset;
+  children?: any;
+};
+
+const exampleToString = (blocks: string | Block | Block[]) => {
+  if (blocks === null) {
+    return 'No Example Available';
+  }
+
   if (isString(blocks)) {
     return blocks;
   }
 
-  // eslint-disable-next-line no-underscore-dangle
-  if (blocks && blocks._type === "slug") {
-    return blocks.current;
-  }
   if (!isArray(blocks)) {
+    if (blocks && blocks._type && blocks._type === 'slug') {
+      return blocks.current;
+    }
+
     return JSON.stringify(blocks);
   }
+
   return map((block) => {
-    // eslint-disable-next-line no-underscore-dangle
-    if (block._type !== "block" || !block.children) {
-      return "<Not yet Implemented>";
+    if (block._type !== 'block' || !block.children) {
+      return '<Not yet Implemented';
     }
-    return map("text", block.children).join("");
-  }, blocks).join("\n\n");
+
+    return map('text', block.children).join('');
+  }, blocks).join('\n\n');
 };
 
 export default exampleToString;
