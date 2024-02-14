23deefaf01989c9ec83454b367e5555059842d1b
Josh Edwards <joshredwards@users.noreply.github.com>
2024-02-14 13:53:29 +1100

    lint fixes for tool index

--- src/components/tool/index.tsx
+++ src/components/tool/index.tsx
@@ -35,6 +35,7 @@ const OptionsJSON = styled.pre`
 
 // Convert the map function to the uncapped version
 // That way we get two parameters for the callback
+// @ts-ignore - it complains about the `convert` function not existing on LodashMapValues
 const map = cappedMap.convert({ cap: false });
 
 const Fieldwork = () => {
@@ -48,72 +49,84 @@ const Fieldwork = () => {
       <h1>Fieldwork</h1>
       <Tagline>Investigate the fields in your schema</Tagline>
       <div>
-        {map((groupOfItems, fieldName) => {
+        {map((groupOfItems: object, fieldName: string) => {
           return (
             <Well key={fieldName}>
               <FieldName>{fieldName}</FieldName>
-              {map((items, hash) => {
-                const { description, options, validation, type } = items[0];
-                return (
-                  <div key={hash}>
-                    {size(groupOfItems) === 1 ? (
-                      <h3>No variations</h3>
-                    ) : (
-                      <h3>
-                        Variation {keys(groupOfItems).indexOf(hash) + 1} of {size(groupOfItems)}
-                      </h3>
-                    )}
-
-                    <h4>Type</h4>
-                    <p>{type}</p>
-                    <h4>Use</h4>
-                    <UseTimes {...{ fieldName, groupOfItems, items }} />
-                    <p>
-                      {map(
-                        ({ typeName }) => (
-                          <Fragment key={typeName}>
-                            <a key={typeName} href={`/desk/${typeName}`}>
-                              {typeName}
-                            </a>{' '}
-                          </Fragment>
-                        ),
-                        sortBy(identity, items),
+              {map(
+                (
+                  items: {
+                    description: any;
+                    options: any;
+                    validation: any;
+                    type: any;
+                  }[],
+                  hash: string,
+                ) => {
+                  const { description, options, validation, type } = items[0];
+                  return (
+                    <div key={hash}>
+                      {size(groupOfItems) === 1 ? (
+                        <h3>No variations</h3>
+                      ) : (
+                        <h3>
+                          Variation {keys(groupOfItems).indexOf(hash) + 1} of {size(groupOfItems)}
+                        </h3>
                       )}
-                    </p>
 
-                    {description && (
-                      <Fragment>
-                        <h4>Description</h4>
-                        <p>{description}</p>
-                      </Fragment>
-                    )}
+                      <h4>Type</h4>
+                      <p>{type}</p>
+                      <h4>Use</h4>
+                      <UseTimes {...{ fieldName, groupOfItems, items }} />
+                      <p>
+                        {map(
+                          // eslint-disable-next-line react/no-unused-prop-types
+                          ({ typeName }: { typeName: string }) => (
+                            <Fragment key={typeName}>
+                              <a key={typeName} href={`/desk/${typeName}`}>
+                                {typeName}
+                              </a>{' '}
+                            </Fragment>
+                          ),
+                          sortBy(identity, items),
+                        )}
+                      </p>
 
-                    {!isEmpty(options) && (
-                      <Fragment>
-                        <h4>Options</h4>
-                        <OptionsJSON>{JSON.stringify(options, null, 2)}</OptionsJSON>
-                      </Fragment>
-                    )}
+                      {description && (
+                        <Fragment>
+                          <h4>Description</h4>
+                          <p>{description}</p>
+                        </Fragment>
+                      )}
 
-                    {!isEmpty(validation) && (
-                      <Fragment>
-                        <h4>Validations</h4>
-                        <p>{validation.join(', ')}</p>
-                      </Fragment>
-                    )}
+                      {!isEmpty(options) && (
+                        <Fragment>
+                          <h4>Options</h4>
+                          <OptionsJSON>{JSON.stringify(options, null, 2)}</OptionsJSON>
+                        </Fragment>
+                      )}
 
-                    <h4>Example</h4>
-                    <p>
-                      <ShowExample
-                        sanityClient={sanityClient}
-                        fieldName={fieldName}
-                        typeNames={map('typeName', items)}
-                      />
-                    </p>
-                    {size(keys(groupOfItems)) === 1 || isEqual(hash, last(keys(groupOfItems))) ? null : <hr />}
-                  </div>
-                );
-              }, groupOfItems)}
+                      {!isEmpty(validation) && (
+                        <Fragment>
+                          <h4>Validations</h4>
+                          <p>{validation.join(', ')}</p>
+                        </Fragment>
+                      )}
+
+                      <h4>Example</h4>
+                      <p>
+                        <ShowExample
+                          sanityClient={sanityClient}
+                          fieldName={fieldName}
+                          typeNames={map('typeName', items)}
+                        />
+                      </p>
+                      {size(keys(groupOfItems)) === 1 || isEqual(hash, last(keys(groupOfItems))) ? null : <hr />}
+                    </div>
+                  );
+                },
+                groupOfItems,
+              )}
             </Well>
           );
         }, viewData)}
